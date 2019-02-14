9597a369c343d7690be9987b0e8b646e43b9da26
tsyaeger <tsyaeger@gmail.com>
2019-02-14 05:14:59 -0800

    refactors create and delete

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -1,45 +1,41 @@
 class Api::V1::RelationshipsController < ApplicationController
 
 	def index
-		# binding.pry
-		@relationships = Relationship.all
-		# @relationships = current_user.contacts
-		# render json: {status: 'SUCCESS', message: "relationships loaded", data: @relationships}, status: :ok
-		render json: @relationships, status: 200
+		# @relationships = Relationship.all
+		@relationships = current_user.contacts
+		json_response(@relationships)
 	end
-
 
 	def show 
 		@relationship = Relationship.find(params[:id])
-		render json: @relationship, status: 200
+		json_response(@relationship)
 	end
 
 	def create
 		# HOW TO USE PRIVAT STONG PARAMS?
-		# binding.pry
 		@savedRelationships = []
 		relationships = params[:relationships]
 		relationships.each do |relationship|
 			# FIND OR CREATE BY
-			# binding.pry
 			@relationship = Relationship.new(:user_id => current_user.id, :contact_id => relationship[:id])
 			if @relationship.save
 				@savedRelationships.push(@relationship)
 				current_user.relationships.push(@relationship)
-				puts "saved"
 			else
 				render json: {status: 'ERROR', message: "unprocessable_entity", data: @relationship.errors}, status: :unprocessable_entity
 			end
 		end
-		binding.pry
-		render json: @savedRelationships, status: 200
+		@new_contacts = @savedRelationships.map {|r| r.contact_id}
+		json_response(@new_contacts)
 	end
 
 
+
 	def destroy
-		@relationship = Relationship.find(params[:id])
-		@relationship.delete
-		render json: {relationshipId: @relationship.id}
+		# binding.pry
+		@relationship = Relationship.find_by(user_id: current_user.id, contact_id: params[:contactId])
+		@relationship.destroy
+		empty_response
 	end
 
 
