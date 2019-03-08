c105e4e1eae6d0b8d9221005c799e6bebadc95b0
tsyaeger <tsyaeger@gmail.com>
2019-03-07 16:49:36 -0800

    cleanup

--- app/serializers/relationship_serializer.rb
+++ app/serializers/relationship_serializer.rb
@@ -1,9 +1,5 @@
 class RelationshipSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :contact_id, :contact
-
- #  	belongs_to :user
-	# belongs_to :contact, :class_name => "User"
 end
 
 
-# contact username
