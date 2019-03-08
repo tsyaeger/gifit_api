571fa5de3db49b216c8552a6b797377fe2143318
tsyaeger <tsyaeger@gmail.com>
2019-03-07 22:30:51 -0800

    refactor responses, strong params, gifs creation

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,27 +2,20 @@ class Api::V1::GifsController < ApplicationController
 
 	def index
 		@gifs = current_user.gifs.order(:created_at)
-		render json: {status: 'SUCCESS', message: "gifs loaded", data: @gifs}, status: :ok
+		json_response(@gifs) || empty_response
 	end
 
 	def show 
 		@gif = Gif.find(params[:id])
-		render json: {status: 'SUCCESS', message: "gif loaded", data: @gif}, status: :ok
+		json_response(@gif) || empty_response
 	end
 
 	def create
-		gifs = params.require(:gifs)
-		savedGifs = []
-		gifs.each do |gif|
-			@gif = Gif.new(gif.permit(:url))
-			if @gif.save
-				savedGifs.push(@gif)
-				current_user.gifs.push(@gif)
-			else
-				render json: {status: 'ERROR', message: "unprocessable_entity", data: @gif.errors}, status: :unprocessable_entity
-			end
-		end
-		render json: {status: 'SUCCESS', message: "gifs created", data: savedGifs}, status: :ok
+		# binding.pry
+		@gifs = Gif.create(gifs_params["gifs_attributes"])
+		@gifs.map { |gif| current_user.gifs.push(gif) }
+
+		json_response(@gifs) || empty_response
 	end
 
 	def destroy
@@ -30,11 +23,8 @@ class Api::V1::GifsController < ApplicationController
 
 		# DELETE ONLY FROM USER GIFS
 		# if current_user.gifs.delete(@gif)
-		if @gif.destroy
-			render json: {status: 'SUCCESS', message: "gif deleted", data: @gif}, status: :ok
-		else
-			render json: {status: 'ERROR', message: "unprocessable_entity", data: @gif.errors}, status: :unprocessable_entity
-		end
+		@gif.destroy
+		empty_response
 	end
 
 
@@ -45,9 +35,7 @@ class Api::V1::GifsController < ApplicationController
 	# end
 
 
-	def gif_params
-		params.require(:gif).permit(:url)
+	def gifs_params
+		params.require(:gif).permit(gifs_attributes: [:url])
 	end
-
-
 end
