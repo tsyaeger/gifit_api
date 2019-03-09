2706c4abb4761539a44d315f112ead926fcb00bc
tsyaeger <tsyaeger@gmail.com>
2019-03-08 23:01:49 -0800

    fixes potential contacts exclusions

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -6,4 +6,5 @@ class Gif < ApplicationRecord
 	has_many :users, through: :user_gifs
 
 	validates :url, format: { with: /(http.*)(.*giphy.gif.cid=*)/} 
+	# validates :url, uniqueness: true #ensure that user still gets gif added rather than error
 end
