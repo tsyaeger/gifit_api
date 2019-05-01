ed1736f1a6a45381c91b8eefee1a772844dc91a8
tsyaeger <tsyaeger@gmail.com>
2019-04-30 19:18:40 -0700

    adds find_or_create gif and gif delete from user library

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -5,35 +5,28 @@ class Api::V1::GifsController < ApplicationController
 		json_response(@gifs) || empty_response
 	end
 
-	def show 
-		@gif = Gif.find(params[:id])
-		json_response(@gif) || empty_response
-	end
-
 	def create
-		# binding.pry
-		@gifs = Gif.create(gifs_params["gifs_attributes"])
-		@gifs.map { |gif| current_user.gifs.push(gif) }
-
-		json_response(@gifs) || empty_response
+		@saved_gifs = []
+		urls = gifs_params["gifs_attributes"].map {|gif| gif[:url]}
+		urls.each do |url|
+			@gif = Gif.find_or_create_by(url: url)
+			if !current_user.gifs.include?(@gif)
+				current_user.gifs.push(@gif)
+			end
+			@saved_gifs.push(@gif)
+		end
+		json_response(@saved_gifs) || empty_response
 	end
 
 	def destroy
 		@gif = Gif.find(params[:id])
-
-		# DELETE ONLY FROM USER GIFS
-		# if current_user.gifs.delete(@gif)
-		@gif.destroy
-		empty_response
+		if current_user.gifs.includes(@gif)
+			current_user.gifs.delete(@gif)
+			empty_response
+		end
 	end
 
-
 	private
-
-	# def set_gif
-	# 	@gif = Gif.find_by(url: params[:url])
-	# end
-
 
 	def gifs_params
 		params.require(:gif).permit(gifs_attributes: [:url])
